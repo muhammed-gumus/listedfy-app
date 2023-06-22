32799964c9fa919511f50f5ad13c726094476079
Muhammed Gümüş <muhammedgumus@Muhammed-MacBook-Air.local>
2023-06-22 11:48:26 +0300

    fixed redirect url

--- spotify/client.js
+++ spotify/client.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { SpotifyApi } from "@spotify/web-api-ts-sdk";
-const callbackUrl = process.env.NODE_ENV === "development" ? "http://localhost:3000/categories" : "https://listedfy-app-git-main-muhammed-gumus.vercel.app/"
+const callbackUrl = process.env.NODE_ENV === "development" ? "http://localhost:3000/categories" : "https://listedfy-app-git-main-muhammed-gumus.vercel.app/categories"
 export const spotifyClient = SpotifyApi.withUserAuthorization(
   "516d129736954da29b9d16c83d9d1e93",
   callbackUrl,
