3e44493db9f7baaa8382b85113ab63ce89406dbd
Muhammed Gümüş <muhammedgumus@Muhammed-MacBook-Air.local>
2023-06-22 11:42:19 +0300

    fixed local and production problem

--- spotify/client.js
+++ spotify/client.js
@@ -1,9 +1,9 @@
 "use client";
 
 import { SpotifyApi } from "@spotify/web-api-ts-sdk";
-
+const callbackUrl = process.env.NODE_ENV === "development" ? "http://localhost:3000/categories" : "https://listedfy-app-git-main-muhammed-gumus.vercel.app/"
 export const spotifyClient = SpotifyApi.withUserAuthorization(
   "516d129736954da29b9d16c83d9d1e93",
-  "http://localhost:3000/categories",
+  callbackUrl,
   []
 );
